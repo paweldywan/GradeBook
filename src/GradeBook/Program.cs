548068c6d7a5bb796882e1272451c999c2e71f9f
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-06-06 05:09:42 +0200

    first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new double[3];
+            numbers[0] = 12.7;
+
+            if(args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
