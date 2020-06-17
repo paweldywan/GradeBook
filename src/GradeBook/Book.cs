c5b5ec4220451ba88b09897a54bcc5d4da34eff9
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-06-18 00:01:18 +0200

    Development

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            Name = name;
+        }
+
+        private List<double> grades;
+        public string Name;
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }    
+
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;   
+            }
+
+            result.Average /= grades.Count;
+
+            return result;
+        }
+    }
+}
